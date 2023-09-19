032b7c6a98a42d11181c77f1703951cf01098407
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-19 18:59:24 -0300

    logica adicionar pessoa

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,37 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner sc = new Scanner(System.in);
 
-        Pessoa p = new Pessoa("Rodrigo Loureiro", formatter.parse("04/06/2001"), 8.5);
+//        Pessoa p = new Pessoa("Rodrigo Loureiro", formatter.parse("04/06/2001"), 8.5);
+//
+//        System.out.println(p);
 
-        System.out.println(p);
+        while (true) {
+            System.out.println("Gerenciador de Aniversários");
+            System.out.println("1- Pesquisar pessoa");
+            System.out.println("2- Adicionar pessoa");
+            System.out.println("3- Sair");
+            int opcao = sc.nextInt();
+            sc.nextLine();
+
+            if (opcao == 3) break;
+            else if (opcao == 2) {
+                System.out.print("Nome completo: ");
+                String nome = sc.nextLine();
+                System.out.print("Data de nascimento: ");
+                String dataNascimento = sc.nextLine();
+                System.out.print("Média final: ");
+                double mediaFinal = sc.nextDouble();
+
+                Pessoa p = new Pessoa(nome, formatter.parse(dataNascimento), mediaFinal);
+            }
+        }
+
+        sc.close();
     }
 }
